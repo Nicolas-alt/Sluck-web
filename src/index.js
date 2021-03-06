f047077e206faf4aa95ca10f47a8ad58b29b5896
Nicolas-alt <a10100105555@gmail.com>
2021-03-06 11:29:14 -0500

    Fix { Scss error }

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import AppRoutes from './routes/AppRoutes';
+import './assets/styles/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
